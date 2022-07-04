1bee0b858e4ee66c83b5186bad60e049dd1ed670
Maverick Wong <mwong@zephyrus14.com>
2022-07-04 03:46:50 -0700

    parse information from current weather API

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,7 +28,7 @@ fetch(geocode)
 
 // //pulls current temp, humidity, windspeed, uv index
 function getCurrent(){
-  var currentWeather = 'https://api.openweathermap.org/data/2.5/onecall?lat='+ lat + '&lon='+ lon +/*'&exclude={part}'*/'&appid=' + APIKey
+  var currentWeather = 'https://api.openweathermap.org/data/2.5/onecall?lat='+ lat + '&lon='+ lon + '&units=imperial' + /*'&exclude={part}'*/ '&appid=' + APIKey
   fetch(currentWeather)
     .then(function (response) {
       return response.json();
@@ -36,13 +36,17 @@ function getCurrent(){
     .then(function (data) {
       console.log("current weather")
       console.log(data);
-
+      console.log("temp : " + data.current.temp);
+      console.log("humidity : " + data.current.humidity)
+      console.log("windspeed : " + data.current.wind_speed)
+      console.log("uvindex : " + data.current.uvi)
+      console.log("Weather : " + data.current.weather[0].description)
     });
   }
 
 // 5 day 
 function getForecast(){
-  var fiveDay = 'http://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&appid='+ APIKey
+  var fiveDay = 'http://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&units=imperial'+ '&appid='+ APIKey
   fetch(fiveDay)
     .then(function (response) {
       return response.json();
@@ -56,13 +60,14 @@ function getForecast(){
 
 /*
   - search 
+        -usser input
         -show history
         -add to history
     - current city , name date, icon for weather conditions
-            -temp
-            -humidity
-            -wind speed
-            -UV index  - color coded
+        ok    -temp
+        ok    -humidity
+        ok    -wind speed
+        ok    -UV index  - color coded
     - 5 day forecast for current city
             -date
             -icon for weather conditions
