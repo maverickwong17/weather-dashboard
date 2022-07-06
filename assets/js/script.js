fd9317b9a80e8c917e32947911b68a0fb4f32f93
MKWong <107278431+maverickwong17@users.noreply.github.com>
2022-07-06 00:46:33 -0700

    5 day forecast functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,24 @@
 // My open weather api key
 var APIKey = "785f7f0c00b9b5585e2d6893363b0aa5"
 
-var lat 
-var lon 
 
 var cityEl = document.querySelector('#city')
 
+var searchInput = document.querySelector('#search')
+var buttonInput = document.querySelector('button')
+var city = ""
+
+buttonInput.addEventListener('click', function(){
+    city = searchInput.value
+    if(city === ""){
+        console.log('input required')
+    }
+    getGeocode()
+} 
+)
+
+var lat 
+var lon 
 //pull geocode data
 function getGeocode() {
     var geocode = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${APIKey}`
@@ -15,14 +28,11 @@ fetch(geocode)
   })
   .then(function (data) {
     console.log(city)
-    console.log("geocode data")
     console.log(data);
     cityEl.textContent = data[0].name 
     lat = data[0].lat;
     lon = data[0].lon;
-    console.log(lat)  
-    console.log(lon)
-    getWeather()
+    getWeather()    
   })
 }
 
@@ -55,45 +65,57 @@ function currentEl(data){
     temp.textContent = "Temperature: " + data.current.temp + "F";
     humidity.textContent = "Humidity: " + data.current.humidity + "%";
     windEl.textContent = "Wind Speed: " + data.current.wind_speed + " MPH";
-    uvi.textContent ="UV Index: " + data.current.uvi;
     currentIcon.setAttribute("src", 'http://openweathermap.org/img/wn/' + data.current.weather[0].icon + '.png')
     currentIcon.setAttribute("alt", 'Current Weather Icon')
+    uvi.textContent ="UV Index: " + data.current.uvi;
 }
 
+var fiveDay = document.querySelector('#five-day')
 // 5 day 
 function getForecast(data){
     for(let i = 1; i < 6; i++){
-      console.log("Day " + i)
-      console.log("dt : " + data.daily[i].dt)
-      console.log("Date: " + moment(data.daily[i].dt*1000).format('dddd, MMMM Do YYYY, h:mm:ss a'))
-      console.log(data.daily[i].weather[0].main)
-      console.log("Temp : " + data.daily[i].temp.day)
-      console.log("Humidity : " + data.daily[i].humidity)
+        //card
+        var cardDiv = document.createElement("div")
+        cardDiv.setAttribute("class", 'card mr-3 pl-2 pr-2')
+        cardDiv.setAttribute("style", 'width: 10rem')
+        //card-body
+        var cardBody = document.createElement("div")
+        cardBody.setAttribute('class', "card-body")
+        //card-title - Date
+        var cardTitle = document.createElement("h5")
+        cardTitle.setAttribute("class", "card-title")
+        cardTitle.textContent = moment(data.daily[i].dt*1000).format("M/D")
+        console.log("Day " + i)
+        console.log("dt : " + data.daily[i].dt)
+        console.log("Date: " + moment(data.daily[i].dt*1000).format('dddd, MMMM Do YYYY, h:mm:ss a'))
+        //icon
+        var cardIcon = document.createElement("img")
+        cardIcon.setAttribute("src", 'http://openweathermap.org/img/wn/' + data.daily[i].weather[0].icon + '.png')
+        currentIcon.setAttribute("alt", 'Weather Icon ' + i)
+        //Temp
+        var cardTemp = document.createElement('p')
+        cardTemp.setAttribute('class','card-text')
+        cardTemp.textContent = "Temp: " + data.daily[i].temp.day
+        //Humidity
+        var cardHumid = document.createElement('p')
+        cardHumid.setAttribute('class','card-text')
+        cardHumid.textContent = "Humidity : " + data.daily[i].temp.day + '%'
+        //append all bootstrap elements
+        fiveDay.appendChild(cardDiv)
+        cardDiv.appendChild(cardBody)
+        cardDiv.appendChild(cardTitle)
+        cardDiv.appendChild(cardIcon)
+        cardDiv.appendChild(cardTemp)
+        cardDiv.appendChild(cardHumid)
     }
   }
 
-var searchInput = document.querySelector('#search')
-var buttonInput = document.querySelector('button')
-var city = ""
 
-buttonInput.addEventListener('click', function(){
-    city = searchInput.value
-    if(city === ""){
-        console.log('input required')
-    }
-    getGeocode()
-} 
-)
 /*
   - search 
         ok    -user input
         -show history
         -add to history
-    - current city , name date, icon for weather conditions
-        ok    -temp
-        ok    -humidity
-        ok    -wind speed
-        ok    -UV index  - color coded
     - 5 day forecast for current city
             -date
             -icon for weather conditions
