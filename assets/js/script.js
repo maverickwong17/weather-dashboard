86301c3fe94a1f30ed3f2d80ff70d44968836ca0
MKWong <107278431+maverickwong17@users.noreply.github.com>
2022-07-06 19:04:29 -0700

    uv index color

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,7 +12,10 @@ buttonInput.addEventListener('click', function(){
     city = searchInput.value
     if(city === ""){
         console.log('input required')
+        return
     }
+
+    //todo add user history
     getGeocode()
 } 
 )
@@ -27,8 +30,6 @@ fetch(geocode)
     return response.json();
   })
   .then(function (data) {
-    console.log(city)
-    console.log(data);
     cityEl.textContent = data[0].name 
     lat = data[0].lat;
     lon = data[0].lon;
@@ -59,35 +60,44 @@ var currentIcon = document.querySelector('#currentIcon')
 
 //current weather elements
 function currentEl(data){
-    console.log("current weather")
-    console.log(data);
     dateEl.textContent = moment().format("L")
     temp.textContent = "Temperature: " + data.current.temp + "F";
     humidity.textContent = "Humidity: " + data.current.humidity + "%";
     windEl.textContent = "Wind Speed: " + data.current.wind_speed + " MPH";
     currentIcon.setAttribute("src", 'http://openweathermap.org/img/wn/' + data.current.weather[0].icon + '.png')
     currentIcon.setAttribute("alt", 'Current Weather Icon')
-    uvi.textContent ="UV Index: " + data.current.uvi;
+    uvi.innerHTML ="UV Index: " + "<span>" + data.current.uvi + "</span>";
+    if (data.current.uvi <= 2){
+        uvi.setAttribute("id", "low")
+    }else if (data.current.uvi <= 5 && data.current.uvi > 2){
+        uvi.setAttribute("id", "moderate")
+    }else if (data.current.uvi <= 7 && data.current.uvi > 5){
+        uvi.setAttribute("id", "high")
+    }else if (data.current.uvi <= 10 && data.current.uvi > 7){
+        uvi.setAttribute("id", "very-high") 
+    }else if (data.current.uvi > 10){
+        uvi.setAttribute("id", "extreme")
+    }
 }
 
 var fiveDay = document.querySelector('#five-day')
-// 5 day 
+// 5 day forecast
 function getForecast(data){
+    while (fiveDay.hasChildNodes()) {
+        fiveDay.removeChild(fiveDay.firstChild);
+      }
     for(let i = 1; i < 6; i++){
         //card
         var cardDiv = document.createElement("div")
-        cardDiv.setAttribute("class", 'card mr-3 pl-2 pr-2')
+        cardDiv.setAttribute("class", 'card mr-1 p-2 bg-primary text-light')
         cardDiv.setAttribute("style", 'width: 10rem')
         //card-body
-        var cardBody = document.createElement("div")
-        cardBody.setAttribute('class', "card-body")
+        // var cardBody = document.createElement("div")
+        // cardBody.setAttribute('class', "card-body")
         //card-title - Date
         var cardTitle = document.createElement("h5")
         cardTitle.setAttribute("class", "card-title")
         cardTitle.textContent = moment(data.daily[i].dt*1000).format("M/D")
-        console.log("Day " + i)
-        console.log("dt : " + data.daily[i].dt)
-        console.log("Date: " + moment(data.daily[i].dt*1000).format('dddd, MMMM Do YYYY, h:mm:ss a'))
         //icon
         var cardIcon = document.createElement("img")
         cardIcon.setAttribute("src", 'http://openweathermap.org/img/wn/' + data.daily[i].weather[0].icon + '.png')
@@ -102,7 +112,7 @@ function getForecast(data){
         cardHumid.textContent = "Humidity : " + data.daily[i].temp.day + '%'
         //append all bootstrap elements
         fiveDay.appendChild(cardDiv)
-        cardDiv.appendChild(cardBody)
+        // cardDiv.appendChild(cardBody)
         cardDiv.appendChild(cardTitle)
         cardDiv.appendChild(cardIcon)
         cardDiv.appendChild(cardTemp)
@@ -113,12 +123,6 @@ function getForecast(data){
 
 /*
   - search 
-        ok    -user input
         -show history
         -add to history
-    - 5 day forecast for current city
-            -date
-            -icon for weather conditions
-            -temp
-            -humidity
 */
